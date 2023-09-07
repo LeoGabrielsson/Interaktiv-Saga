242684c89d45b2947a936e0b039ec3d30502a9ff
= <=>
2023-09-07 10:50:53 +0200

    🚧 🎨

---
+++ code.js
@@ -0,0 +1,9 @@
+export function site(element) {
+
+  start.addEventListener('click', (e) => {
+    //const start = element.querySelector('#start').value;
+    const readable = element.querySelector('#story');
+    //readable.innerHTML = intro
+    console.log(intro())
+  })
+}
--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 import './style.css'
+import {site} from './code.js'
 
-import { setupCounter } from './counter.js'
+document.querySelector('#app').innerHTML = `
+<h1>Leo, Imperial Guardsman</h1>
+<p id="story">This is the interactive story of Leo and his life in the "Astra Millitarum".
+<br>Grab a seat and we will dive headfirst into the grimdark</p>
+<button id="start">Grab a seat</button>
+`
+site(document.querySelector(`#app`))
