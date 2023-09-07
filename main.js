1567fb7b55491f02dbc4503ca23a367157aa044d
= <=>
2023-09-07 15:11:54 +0200

    🚧

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ import {site} from './protagonist.js'
 document.querySelector('#app').innerHTML = `
 <h1>Leo, Imperial Guardsman</h1>
 <p id="story"></p>
-<button id="start">Grab a seat</button>
+<div id="buttons">
+    <button id="start">Grab a seat</button>
+</div>
 `
 site(document.querySelector(`#app`))
--- protagonist.js
+++ protagonist.js
@@ -2,8 +2,10 @@ import { storySegements } from "./storyParts.js";
 export function site(element) {
   const readable = element.querySelector('#story');
   readable.innerHTML = storySegements[0].Text
+  let buttonlist = element.querySelector('#buttons')
   start.addEventListener('click', (e) => {
     readable.innerHTML = storySegements[1].Text
+    const buttonFactorium = document.createElement('button')
+    
   })
 }
-
