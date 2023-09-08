f3715ddd0c4e22eb40e1adbefd3a369ba6c310e5
= <=>
2023-09-08 10:29:43 +0200

    Added a button factory and formatted code

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 import './style.css'
-import {site} from './protagonist.js'
+import { site } from './protagonist.js'
 
 document.querySelector('#app').innerHTML = `
 <h1>Leo, Imperial Guardsman</h1>
 <p id="story"></p>
-<div id="buttons">
-    <button id="start">Grab a seat</button>
-</div>
+<div id="buttons"></div>
 `
 site(document.querySelector(`#app`))
--- protagonist.js
+++ protagonist.js
@@ -1,11 +1,29 @@
 import { storySegements } from "./storyParts.js";
+
+
+
 export function site(element) {
-  const readable = element.querySelector('#story');
-  readable.innerHTML = storySegements[0].Text
-  let buttonlist = element.querySelector('#buttons')
-  start.addEventListener('click', (e) => {
-    readable.innerHTML = storySegements[1].Text
-    const buttonFactorium = document.createElement('button')
+  const buttonList = document.getElementById('buttons')
+  const storyText = element.querySelector('#story');
+  let currentEvent = 0
+  storyText.innerHTML = storySegements[currentEvent].Text
+  buttonMaker(storySegements[currentEvent].Buttons)
+
+  buttons.addEventListener('click', (e) => {
+    currentEvent += storySegements[currentEvent].Next
+    buttonList.innerHTML = ""
+    storyText.innerHTML = storySegements[currentEvent].Text
+    buttonMaker(storySegements[currentEvent].Buttons)
     
   })
+
+  function buttonMaker(x) {
+    for (let z = 0; z < x; z++) {
+      let makeButton = document.createElement('button')
+      let buttonText = document.createTextNode(storySegements[currentEvent].Options[z])
+      buttonList.appendChild(makeButton)
+      makeButton.appendChild(buttonText)
+
+    }
+  }
 }
