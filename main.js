61783ddaef7df8d985e52ca1effa5a7307df64dd
= <=>
2023-09-09 15:06:56 +0200

    Reformatted code

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ import { site } from './protagonist.js'
 document.querySelector('#app').innerHTML = `
 <h1>Leo, Imperial Guardsman</h1>
 <p id="story"></p>
-<div id="buttons"></div>
+<div id="buttons">
+    <div id="B1"></div>
+    <div id="B2"></div>
+</div>
 `
 site(document.querySelector(`#app`))
