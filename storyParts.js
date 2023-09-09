20f4420cc2492ba3e0b8802d139ab1443aa7f36d
= <=>
2023-09-09 15:07:16 +0200

    Added endings Reformatted code

--- storyParts.js
+++ storyParts.js
@@ -1,8 +1,11 @@
 export let storySegements = [
     {
-        "Text": `This is the interactive story of Leo and his life in the "Astra Millitarum".
+        "Text": `This is the interactive story of Leo and his life in the 718 Regiment in the "Astra Millitarum".
         Grab a seat and we will dive headfirst into the grimdark`,
-        "Options":"Grab a seat"
+        "Options": [`Grab a seat`],
+        "Buttons": 1,
+        "ButtonID": [0],
+        "Next": [1]
     },
     {
         "Text": `In the grim and distant future of the 41st millennium, the Imperium of Man stood as a beacon of humanity amidst the darkness of the galaxy. War and chaos reigned across the stars, and it was the duty of countless soldiers, like Imperial Guardsman Leo, to defend their species from the unrelenting horrors that lurked beyond the stars.
@@ -11,9 +14,43 @@ export let storySegements = [
             
             On a desolate world at the fringe of the Imperium, Leo and his regiment were dispatched to investigate a distress signal from an isolated outpost. The planet, Tarsis IV, was a barren wasteland known for its eerie silence, but the signal had come from a remote research facility that had been studying ancient xenos artifacts.
             
-            As the Guardsmen approached the facility, the oppressive silence was shattered by the discordant roar of chittering and screeching.`,
-        "Options":'Ready lasgun and proceed',
+            As the Guardsmen approached the facility, the oppressive silence was shattered by the discordant roar of chittering and screeching.
+            Unsure of how to proceed, they sent a prayer to the God-Emporor of Mankind asking what they should do.`,
+        "Options": [`Ready lasgun and proceed`, `Continue forward. It's problaly just a few muta-rats`],
+        "Buttons": 2,
+        "Next": [2, 5]
     },
     {
-        "Text": '',
+        "Text": `The guardsmen readied their lasguns, their hearts pounding as they advanced cautiously through the twisted metal ruins. The air was thick with tension as they rounded a corner, and there, illuminated by flickering emergency lights, they saw the nightmare that awaited them.
+        
+        Tyranids, the ravenous alien swarm that consumed entire worlds, had descended upon Tarsis IV. The monstrous creatures were a grotesque amalgamation of insectoid and reptilian features, with razor-sharp talons and gaping maws filled with rows of serrated teeth. Leo's stomach churned at the sight, for he had heard tales of the Tyranids but had never faced them in battle.
+        Leo could hear the Emporor whisper in his mind to:`,
+        "Options": [`Open fire`, `Run away`],
+        "Buttons": 2,
+        "Next": [3, 4]
+    },
+    {
+        "Text": `The Imperial Guardsmen opened fire, lasgun beams lancing into the approaching swarm. But for every Tyranid they felled, it seemed two more took their place. Leo and his comrades fought with grim determination, their lasguns blazing as they formed a defensive line, but it was clear that they were outnumbered and outgunned.
+        As the battle raged on, Leo's mind raced. He knew that the only chance for survival lay in reaching the facility's communication center to send a distress signal to their orbital support. With their commisar dead, Leo decidet to take the initiative and with a shouted order, he led a small group of Guardsmen through the sea of chittering horrors, their bayonets fixed and lasguns firing.
+
+        The journey through the labyrinthine facility was a harrowing ordeal. Tyranids lurked around every corner, and Leo's group was forced to make strategic retreats and fight desperate last stands. But their determination held, and they finally reached the communication center. With shaking hands, Leo activated the distress beacon, sending a signal that pierced the void of space and reached the orbital fleet. Reinforcements were on their way, but time was running out. The Tyranid swarm pressed closer, their relentless advance unyielding.
+        
+        The legend of the 718 say that they are still fighting to this day in wait of support from the Imperium.`
+    },
+    {
+        "Text": `The swarm of tyranids reacted to the smell of new prey as Leo and his brothers in arms turned the corner. They all roard in unison, a roar that easily could be understod by any living being as "The hund is on!" and swiftly started running towards Leo and his comrades. In response to this, Leo and his comrades turned tail ran away with all their might from the tyranid swarm.
+        Unfortunately for the 718tg Regiment, cowardice was not allowed in the Imperium of Man. Every regiment's commissar have been trained and ordered to shoot fleeing and/or deserting troops. The commissar shouting of bravery in the face of death was almost drowned out by the sounds of his laspistol as he fired round after round of shots into the fleeing guardsmen.
+        Only a few shots was fired before the commisar was devoured by the many maws of the swarm. But only a few shots was all that was required to end the last men of the 718th Regiment. And only a few and thus the story of Leo came to and end. Shot by his own commisar, the story of Leo and the 718 Regiment ends`,
+    },
+    {
+        "Text": `As the guardsmen pushed through the darkness, trying not to mind the ever increasing loudness from the presumed to be muta-rats. As Leo and his brothers turnd a corner they saw to to their horrific suprise the source of the chittering.
+        
+        It was the ravenous alien swarm that consumed entire worlds, the Tyranids that had descended upon Tarsis IV. The monstrous creatures were a grotesque amalgamation of insectoid and reptilian features, with razor-sharp talons and gaping maws filled with rows of serrated teeth. Leo's stomach churned at the sight, for he had heard tales of the Tyranids but had never faced them in battle.
+        
+        The swarm of tyranids reacted to the smell of new prey as Leo and his brothers in arms turned the corner. They ceased their feeding on what seemed to be a mountain of red flesh as they all fell silent. Then, they all roared in unison, a roar that easily could be understod by any living organism as "The hund is on!" and swiftly started running towards Leo and his comrades.
+        
+        His commisar shouted with a strict and slightly panicked voice "MEN! READY LASGUNS, FIX BAYONETS AND OPEN FIRE! IN THE NAME OF THE EMPOROR WE WILL ERADICATE THE FILTH ON THIS PLANET AND RECLAME IT FOR THE THRONE!". Leo started to panic as he fumbled to try and ready his lasgun, the swarm approaching ever faster. A onesided massacre ensued. Roaring lasfire clashing against tyranid screeching. Leo and his regiment was quicly overwhelmed by the mass of Tyranid bodies that slammed into them.
+        And so the story of Leo and the 718 Regiment ends, ravenously feasted upon by the Tyranids.`,
+        "Options": [`Open fire`,],
+        "Buttons": 0,
     }]
