aa53840d3731282e1a9453f094de89b6fa05e72e
= <=>
2023-09-09 15:09:16 +0200

    FINALLY fixed the buttons not working/showing up Cleaned up code

--- protagonist.js
+++ protagonist.js
@@ -3,27 +3,41 @@ import { storySegements } from "./storyParts.js";
 
 
 export function site(element) {
-  const buttonList = document.getElementById('buttons')
   const storyText = element.querySelector('#story');
   let currentEvent = 0
   storyText.innerHTML = storySegements[currentEvent].Text
   buttonMaker(storySegements[currentEvent].Buttons)
 
-  buttons.addEventListener('click', (e) => {
-    currentEvent += storySegements[currentEvent].Next
-    buttonList.innerHTML = ""
+  B1.addEventListener('click', (e) => {
+    currentEvent = storySegements[currentEvent].Next[0]
     storyText.innerHTML = storySegements[currentEvent].Text
+    B1.innerHTML = ""
+    B2.innerHTML = ""
     buttonMaker(storySegements[currentEvent].Buttons)
-    
+
+  })
+  B2.addEventListener('click', (e) => {
+    currentEvent = storySegements[currentEvent].Next[1]
+    storyText.innerHTML = storySegements[currentEvent].Text
+    B1.innerHTML = ""
+    B2.innerHTML = ""
+    buttonMaker(storySegements[currentEvent].Buttons)
+
   })
 
   function buttonMaker(x) {
     for (let z = 0; z < x; z++) {
       let makeButton = document.createElement('button')
       let buttonText = document.createTextNode(storySegements[currentEvent].Options[z])
-      buttonList.appendChild(makeButton)
-      makeButton.appendChild(buttonText)
-
+      if (z < 1) {
+        B1.appendChild(makeButton)
+        makeButton.appendChild(buttonText)
+      }
+      else {
+        B2.appendChild(makeButton)
+        makeButton.appendChild(buttonText)
+      }
     }
   }
 }
+
