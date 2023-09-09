77816e160308b5577961e29bec6deccec86da331
= <=>
2023-09-09 15:10:15 +0200

    Added patch indicator

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import { site } from './protagonist.js'
 
 document.querySelector('#app').innerHTML = `
 <h1>Leo, Imperial Guardsman</h1>
+<p>Patch 1.0</p>
+<br>
 <p id="story"></p>
 <div id="buttons">
     <div id="B1"></div>
