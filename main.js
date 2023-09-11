d72a8df2e1d91a170ecfcec1ad079c4c78641330
= <=>
2023-09-11 13:41:38 +0200

    Added a "Return to start button"

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ import { site } from './protagonist.js'
 
 document.querySelector('#app').innerHTML = `
 <h1>Leo, Imperial Guardsman</h1>
-<p>Patch 1.0</p>
+<p class="patch">Patch 1.0</p>
+<div class="backButton" id="backButton">
+</div>
 <br>
-<p id="story"></p>
+<p class="mainText" id="story"></p>
 <div id="buttons">
     <div id="B1"></div>
+    <br>
     <div id="B2"></div>
 </div>
 `
--- protagonist.js
+++ protagonist.js
@@ -3,10 +3,13 @@ import { storySegements } from "./storyParts.js";
 
 
 export function site(element) {
-  const storyText = element.querySelector('#story');
+  const storyText = element.querySelector('#story')
+  const backButton = element.querySelector('#backButton')
   let currentEvent = 0
   storyText.innerHTML = storySegements[currentEvent].Text
   buttonMaker(storySegements[currentEvent].Buttons)
+
+
 
   B1.addEventListener('click', (e) => {
     currentEvent = storySegements[currentEvent].Next[0]
@@ -14,6 +17,7 @@ export function site(element) {
     B1.innerHTML = ""
     B2.innerHTML = ""
     buttonMaker(storySegements[currentEvent].Buttons)
+    runItBack()
 
   })
   B2.addEventListener('click', (e) => {
@@ -22,6 +26,7 @@ export function site(element) {
     B1.innerHTML = ""
     B2.innerHTML = ""
     buttonMaker(storySegements[currentEvent].Buttons)
+    runItBack()
 
   })
 
@@ -38,6 +43,21 @@ export function site(element) {
         makeButton.appendChild(buttonText)
       }
     }
+  }
+  function runItBack() {
+    let makeButton = document.createElement('button')
+    let buttonText = document.createTextNode("Back to Start")
+    backButton.appendChild(makeButton)
+        makeButton.appendChild(buttonText)
+        makeButton.appendChild(buttonText)
+    backButton.addEventListener('click', (e) => {
+      currentEvent = 0
+      storyText.innerHTML = storySegements[currentEvent].Text
+      backButton.innerHTML = ""
+      B1.innerHTML = ""
+      B2.innerHTML = ""
+      buttonMaker(storySegements[currentEvent].Buttons)
+    })
   }
 }
 
