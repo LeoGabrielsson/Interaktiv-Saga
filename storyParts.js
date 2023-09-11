e7d4caf61715407d8d32d9d0f45d4b3c0c09fe92
= <=>
2023-09-11 14:27:10 +0200

    Added a missing word

--- storyParts.js
+++ storyParts.js
@@ -10,7 +10,7 @@ export let storySegements = [
     {
         "Text": `In the grim and distant future of the 41st millennium, the Imperium of Man stood as a beacon of humanity amidst the darkness of the galaxy. War and chaos reigned across the stars, and it was the duty of countless soldiers, like Imperial Guardsman Leo, to defend their species from the unrelenting horrors that lurked beyond the stars.
 
-            Leo was a battle-hardened veteran of the Cadian 718th Regiment, a world famous for its unyielding resolve in the face of chaos. His rugged features bore the scars of countless battles against the xenos forces, and his eyes held the grim determination that came from witnessing the darkest horrors of the galaxy.
+            Leo was a battle-hardened veteran of the Cadian 718th Regiment, a world famous unit for its unyielding resolve in the face of chaos. His rugged features bore the scars of countless battles against the xenos forces, and his eyes held the grim determination that came from witnessing the darkest horrors of the galaxy.
             
             On a desolate world at the fringe of the Imperium, Leo and his regiment were dispatched to investigate a distress signal from an isolated outpost. The planet, Tarsis IV, was a barren wasteland known for its eerie silence, but the signal had come from a remote research facility that had been studying ancient xenos artifacts.
             
